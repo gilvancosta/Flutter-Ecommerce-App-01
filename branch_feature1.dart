820bf4d48c01e3d3cb14585aab502ce8d7bc7d12
Gilvan Costa <gilvancosta2025@gmail.com>
2023-12-20 11:47:35 -0300

    new branch feature1

---
+++ branch_feature1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BranchFeature1 extends StatelessWidget {
+
+  const BranchFeature1({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Branch Feature 1'),),
+           body: Container(),
+       );
+  }
+}
